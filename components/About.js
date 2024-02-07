dafb610010eea16acb20fc346b2227a15cb03234
martinpoehl <martinpoehl@me.com>
2024-02-07 16:15:06 +0100

    aboutMe

--- components/About.js
+++ components/About.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div id='contact' className="w-full justify-center mt-40">
+    <div id='contact' className="w-full justify-center mt-40 mb-20">
       <div class="bg-gray-100">
     <div class="container mx-auto md:p-10">
         <div class="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
             <div class="col-span-4 sm:col-span-3">
-                <div class="bg-white shadow rounded-lg p-6">
+                <div class="bg-white-500 shadow rounded-lg p-6">
                     <div class="flex flex-col items-center">
-                        <img src="/assets/me.jpg" class="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0">
+                        <img src="/assets/me.jpg" class="w-40 h-40 bg-gray-300 rounded-full mb-4 shrink-0">
 
                         </img>
                         <h1 class="text-xl font-bold">Martin Pöhl</h1>
-                        <p class="text-gray-700">Webdeveloper, System Engeener</p>
+                        <p class="text-gray-700 mt-3">Webdeveloper &<br></br> System Engeener</p>
                         <div class="mt-6 flex flex-wrap gap-4 justify-center">
                             <a href="#" class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Contact</a>
                             <a href="#" class="bg-gray-300 hover:bg-gray-400 text-gray-700 py-2 px-4 rounded">Resume</a>
@@ -33,7 +33,7 @@ const About = () => {
                 </div>
             </div>
             <div class="col-span-4 sm:col-span-9">
-                <div class="bg-white shadow rounded-lg p-6">
+                <div class="bg-white-500 shadow rounded-lg p-6">
                     <h2 class="text-xl font-bold mb-4">Über mich</h2>
                     <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus est
                         vitae tortor ullamcorper, ut vestibulum velit convallis. Aenean posuere risus non velit egestas
