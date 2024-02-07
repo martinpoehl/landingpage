e585f0e122bc7b4dc6a116ac8331a0fc076f85dd
martinpoehl <martinpoehl@me.com>
2024-02-07 12:50:07 +0100

    footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -8,8 +8,8 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className="bg-gray-100 pt-48 pb-10 ">
-      <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 mb-10 lg:px-16 grid grid-rows-3 sm:grid-rows-1 grid-flow-row sm:grid-cols-5 grid-rows-1 gap-2">
-        <div className="row-span-1 sm:col-span-4 col-start-1 col-end-2 sm:col-end-5 flex flex-col items-start">
+      <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 mb-10 lg:px-16 grid grid-rows-2 sm:grid-rows-1 grid-flow-row sm:grid-cols-5 grid-rows-1 gap-2">
+        <div className="row-span-1 sm:col-span-2 col-start-1 col-end-2 sm:col-end-2 flex flex-col items-start">
           <Image
             className="shadow-2xl"
             src={Logo}
@@ -24,13 +24,10 @@ const Footer = () => {
           </p>
           <div className="flex w-full mt-2 mb-8 -mx-2">
             <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
-              <Github className="h-6 w-6" />
-            </div>
-            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
-              <Twitter className="h-6 w-6" />
-            </div>
-            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
               <Instagram className="h-6 w-6" />
+            </div>
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Github className="h-6 w-6" />
             </div>
           </div>
           <p className="text-gray-400">
