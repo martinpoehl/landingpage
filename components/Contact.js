848a0f37c6fa9bde8bd25a72108989815e433c1e
martinpoehl <martinpoehl@me.com>
2024-03-06 17:35:38 +0100

    contact

--- components/Contact.js
+++ components/Contact.js
@@ -12,10 +12,11 @@ const Contact = () => {
   });
   const [submitted, setSubmitted] = useState(false);
 
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post('/api/submit-form.js', formData);
+      await axios.post('/api/contact', formData);
       setSubmitted(true);
     } catch (error) {
       console.error('Form submission error:', error);
@@ -58,7 +59,7 @@ const Contact = () => {
               </div>
               <div className="sm:col-span-2">
                 <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nachricht</label>
-                <textarea name="message" id="message" rows="6" value={formData.message} onChange={handleChange} className="px-4 py-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="z.B Ich hätte gerne eine Beratung"></textarea>
+                <textarea name="message" id="message" rows="6" value={formData.message} onChange={handleChange} className="px-4 py-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Ich hätte gerne eine Beratung für ..."></textarea>
               </div>
               <ButtonPrimary type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Senden</ButtonPrimary>
             </form>
