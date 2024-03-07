b30e74fcf4f64662309a21bbe8f49e50febb39ea
martinpoehl <martinpoehl@me.com>
2024-03-07 08:54:07 +0100

    form working

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,44 +1,36 @@
-import nodemailer from 'nodemailer';
+import sgMail from '@sendgrid/mail';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { firstName, lastName, email, phone, message } = req.body;
 
-    // Create a transporter object using SMTP transport
-    const transporter = nodemailer.createTransport({
-      host: 'asmtp.mail.hostpoint.ch',
-      port: 25,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-      }
-    });
+    // Set your SendGrid API key
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
     // Setup email data
-    const mailOptions = {
+    const msg = {
+      to: 'martinpoehl@me.com', // recipient email
       from: 'info@martinpoehl.me',
-      to: 'martinpoehl@me.com',
       subject: 'Formular: Landingpage',
       text: `
         Vorname: ${firstName}
         Nachname: ${lastName}
         Email: ${email}
         Telefonnummer: ${phone}
-        Nachricht: ${message}
+        Nachricht: 
+        ${message}
       `
     };
 
     // Send email
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error('Email sending error:', error);
-        res.status(500).json({ error: 'Failed to submit form' });
-      } else {
-        console.log('Email sent:', info.response);
-        res.status(200).json({ message: 'Form submitted successfully' });
-      }
-    });
+    try {
+      await sgMail.send(msg);
+      console.log('Email sent successfully');
+      res.status(200).json({ message: 'Form submitted successfully' });
+    } catch (error) {
+      console.error('Email sending error:', error.response.body.errors);
+      res.status(500).json({ error: 'Failed to submit form' });
+    }
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
   }
