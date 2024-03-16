749a2c63400d53f37f39700ab097ac5fab000182
Martin P <45636696+martinpoehl@users.noreply.github.com>
2024-03-16 18:39:21 +0100

    Title

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import Projects from "../components/Projects";
 export default function Home() {
   return (
     <>
-      <SeoHead title="Martin Pöhl - Lassen Sie sich eine professionelle Webseite erstellen" />
+      <SeoHead title="MartinPoehl - Erstellen und Hosting von Webseiten & Webapplikationen" />
       <Layout>
         <Hero />
         <Products />
