d2d8751ac79c09ee685f98553c1e33a0858d6352
Martin P <45636696+martinpoehl@users.noreply.github.com>
2024-03-17 11:46:43 +0100

    google analytics in head

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,17 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 export default function RootLayout({ children }) {
   return (
     <html lang="de">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-C4R57RK53J"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-C4R57RK53J');
+        </script>
+      </head>
       <body>{children}</body>
       <GoogleAnalytics gaId="G-C4R57RK53J" />
     </html>
