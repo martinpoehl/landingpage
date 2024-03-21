0a4c902dbbd55b23168cda586225732f3722b811
martinpoehl <martinpoehl@me.com>
2024-03-21 10:26:32 +0100

    gtag

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -49,6 +49,16 @@ const SeoHead = (props) => {
   return (
     <Head>
 
+      {/* Google tag (gtag.js) */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-C4R57RK53J"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'G-C4R57RK53J');
+      </script>
+
       <title>{meta.title}</title>
 
       <meta name="robots" content={meta.robots} />
