dfeffe3b73f5247826f2172c3013581567560178
Martin P <45636696+martinpoehl@users.noreply.github.com>
2024-03-24 11:20:03 +0100

    gtag

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -57,9 +57,11 @@ const SeoHead = (props) => {
         src="https://www.googletagmanager.com/gtag/js?id=G-C4R57RK53J"
       ></script>
       <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'G-C4R57RK53J');
+        {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-C4R57RK53J');`}
       </script>
 
       <meta name="robots" content={meta.robots} />
